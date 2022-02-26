ba66ccadfcdbb1cf7d77391f212b6d532d833bb9
Askar <askar_1996@list.ru>
2022-02-26 19:36:18 +0600

    Первое задание замена классов на функции

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,32 +1,32 @@
 import React from 'react';
 import AppHeader from '../app-header/app-header';
-import {data} from '../../utils/data.js';
+import { data } from '../../utils/data.js';
 import app from './app.module.css';
 import BurgerIngredients from '../burger-content/burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-content/burger-constructor/burger-constructor';
 
 
-class App extends React.Component { 
-  constructor(props:any) {
-    super(props);
-    this.state = {
-      product : {data}
-    }
-  }
- 
-  render() {
+function App() {
+  // constructor(props: any) {
+  //   super(props);
+  //   this.state = {
+  //     product: { data }
+  //   }
+  // }
+  const [state, setState] = React.useState();
+
   return (
-      <>
-        <AppHeader/>
-        <div className={`${app.container}`}>
-            <div className={`${app.flex} ${app.justify_between}`}>
-              <BurgerIngredients card={data}/>
-              <BurgerConstructor  card={data} />
-          </div>
+    <>
+      <AppHeader />
+      <div className={`${app.container}`}>
+        <div className={`${app.flex} ${app.justify_between}`}>
+          <BurgerIngredients card={data} />
+          <BurgerConstructor card={data} />
         </div>
-      </>
-    );
-  }
-} 
+      </div>
+    </>
+  );
+}
+
 
 export default App;
