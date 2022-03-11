341b9cb0d3a6d59fd767dbbb98c772ac23c709e8
Askar <askar_1996@list.ru>
2022-03-11 11:16:54 +0600

    поправил const вместо let

--- src/components/order-details/total-price/total-price.jsx
+++ src/components/order-details/total-price/total-price.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 function TotalPrice(props) {
 
-   const totalBun = 0;
+   let totalBun = 0;
    props.bun.map(item => (totalBun += item.price));
 
-   const totalOther = 0;
+   let totalOther = 0;
    props.ingredients.map(item => (totalOther += item.price));
 
-   const total = (totalBun * 2) + totalOther;
+   let total = (totalBun * 2) + totalOther;
    return (
       <span className='mr-2'>{total}</span>
    );
